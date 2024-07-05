8821b6815417be9db57e9b06a98a6aee3e16b49a
Azazek6 <naturaespinola12@gmail.com>
2024-07-04 23:41:59 -0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CardRight from "./components/CardRight";
 
 const App = () => {
   return (
-    <div className="sm:h-screen flex flex-col sm:justify-center sm:items-center xs:m-[3%] sm:m-[10%]">
+    <div className="sm:h-screen flex flex-col sm:justify-center sm:items-center xs:m-[3%] sm:m-[10%] md:m-[3%]">
       <div className="flex flex-col md:flex-row p-0  bg-[hsl(0,0%,100%)] xs:rounded-t-2xl md:rounded-t-none md:rounded-l-2xl">
         <CardLeft />
         <CardRight />
